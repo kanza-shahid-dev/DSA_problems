236b0d3db71b237a6bba3a2a3392ca36455bd777
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-02-24 23:51:11 +0500

    Valid Parentheses

---
+++ ValidParentheses.js
@@ -0,0 +1,21 @@
+var isValid = function (s) {
+  let map = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  let stack = [];
+
+  for (let char of s) {
+    //opening
+    if (char === "(" || char === "{" || char === "[") stack.push(char);
+    //closing
+    else {
+      let top = stack.pop();
+      if (top !== map[char])
+        // '('
+        return false;
+    }
+  }
+  return stack.length === 0;
+};
