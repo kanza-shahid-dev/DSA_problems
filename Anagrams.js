842a8dd78571c972a9e2ce720454b351928bcfcd
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-02-25 23:43:26 +0500

    isAnagram

--- Anagrams.js
+++ Anagrams.js
@@ -2,7 +2,17 @@
 let s1 = "geeks";
 let s2 = "kseeg";
 
-const isAnn = (s1, s2) => {
+var isAnagram = function (s1, s2) {
+  if (s1.length !== s2.length) return false;
+
+  // Sorting both strings
+  s1 = s1.split("").sort().join("");
+  s2 = s2.split("").sort().join("");
+
+  // Comparing sorted strings
+  return s1 === s2;
+};
+const isAnagramWithoutSort = (s1, s2) => {
   let freq = {};
   for (let char of s1) {
     freq[char] = (freq[char] || 0) + 1;
