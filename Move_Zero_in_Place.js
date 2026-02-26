81d2bc8b08fcf6e49de96e0974310cb828697282
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-02-27 03:50:15 +0500

    Move zero

--- Move_Zero_in_Place.js
+++ Move_Zero_in_Place.js
@@ -6,7 +6,7 @@ var moveZeroes = function (nums) {
   let left = 0;
 
   for (let right = 0; right < nums.length; right++) {
-    if (nums[right] !== 0) {
+    if (nums[right] != 0) {
       [nums[left], nums[right]] = [nums[right], nums[left]];
       left++;
     }
