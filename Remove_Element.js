48bb74901671a919fca2b09a41fdebb2391d39c8
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-02-27 03:50:25 +0500

    Remove element

--- Remove_Element.js
+++ Remove_Element.js
@@ -1,27 +1,13 @@
+/********* Two Pointer Approach *********/
 var removeElement = function (nums, val) {
-  let left = 0; // pointer for placing non-val elements
+  let left = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      console.log(nums[i], val);
-      nums[left] = nums[i];
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] !== val) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
       left++;
     }
   }
 
-  return left;
-};
-
-/********* Two Pointer Approach *********/
-var removeElement = function (nums, val) {
-  let left = 0;
-  let right = nums.length - 1;
-
-  while (left <= right) {
-    if (nums[left] === val) {
-      nums[left] = nums[right];
-      right--;
-    } else left++;
-  }
   return left;
 };
