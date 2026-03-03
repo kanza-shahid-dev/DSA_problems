85644f120ed7a3bde9104a97dc92c63e5fcf6a0f
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-03-03 23:36:55 +0500

    optimal sol

--- Find_All_Missing_Numbers.js
+++ Find_All_Missing_Numbers.js
@@ -1,20 +1,21 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var findDisappearedNumbers = function (nums) {
   let res = [];
-  let sorted = nums.sort((a, b) => a - b);
 
-  let arr = [...new Set(sorted)];
-  console.log(arr);
-
+  // Mark visited numbers
   for (let i = 0; i < nums.length; i++) {
-    console.log("n", arr[i], "i", i + 1);
-    if (arr[i] != i + 1) {
-      console.log("not", arr[i], i + 1);
+    let index = Math.abs(nums[i]) - 1;
+
+    if (nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
+  }
+
+  // Collect missing numbers
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
       res.push(i + 1);
     }
   }
+
   return res;
 };
