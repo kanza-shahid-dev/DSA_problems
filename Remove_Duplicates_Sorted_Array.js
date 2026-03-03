309af755451fd99287d02b1ff32fde376cd5d855
kanza-shahid-dev <kanzashahid18@gmail.com>
2026-03-03 22:13:38 +0500

    removeDuplicates

--- Remove_Duplicates_Sorted_Array.js
+++ Remove_Duplicates_Sorted_Array.js
@@ -2,7 +2,7 @@ var removeDuplicates = function (nums) {
   // two pointer
   let left = 0;
 
-  for (let right = 1; right < nums.length; right++) {
+  for (let right = 0; right < nums.length; right++) {
     if (nums[left] !== nums[right]) {
       left++;
       nums[left] = nums[right];
